c2f38ab686829c4de584a10fa5973d17853c077d
Vahe9955 <vahe99551@gmail.com>
2023-11-11 20:56:14 +0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LogixUserManagmentApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
